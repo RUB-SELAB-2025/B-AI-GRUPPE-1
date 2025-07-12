03d682fd245bfb372ece75bb397866a99e7666f1
Eric Wefelnberg <eric.wefelnberg@gmail.com>
2025-07-12 09:32:09 +0200

    adding the unit tests

--- angular-frontend/src/app/graph/graph.component.spec.ts
+++ angular-frontend/src/app/graph/graph.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GraphComponent } from './graph.component';
 import {provideHttpClient} from '@angular/common/http';
+import { DataSourceService } from './graph-data.service';
 
 describe('GraphComponent', () => {
   let component: GraphComponent;
@@ -11,7 +11,8 @@ describe('GraphComponent', () => {
     await TestBed.configureTestingModule({
       imports: [GraphComponent],
       providers: [
-        provideHttpClient()
+        provideHttpClient(),
+        { provide: DataSourceService, useValue: {}}
       ]
     }).compileComponents();
 
@@ -22,5 +23,73 @@ describe('GraphComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submit a comment', () => {
+    component.comments.set([]);
+    component.commentInputText.set('Testkommentar');
+    component.submitComment();
+
+    const lastComment = component.comments().at(-1);
+    expect(lastComment?.text).toBe('Testkommentar');
+    expect(component.commentInputVisible()).toBeFalse();
+  });
+
+  it('should cancel comment input', () => {
+    component.commentInputText.set('Abbrechen');
+    component.commentInputVisible.set(true);
+    component.cancelComment();
+    expect(component.commentInputVisible()).toBeFalse();
+    expect(component.commentInputText()).toBe('');
+  });
+
+  it('should not submit empty comment', () => {
+    component.comments.set([]);
+    component.commentInputText.set('');
+    component.submitComment();
+
+    expect(component.comments().length).toBe(0); 
+  });
+
+  it('should remove hovered guide on Backspace', () => {
+    const fixture = TestBed.createComponent(GraphComponent);
+    const component = fixture.componentInstance;
+
+    component.fixedGuides.set([10, 20]);
+    component.hoveredGuide.set(10);
+
+    const event = new KeyboardEvent('keydown', { key: 'Backspace' });
+    const preventSpy = spyOn(event, 'preventDefault');
+
+    component.onKeyDown(event);
+
+    expect(component.fixedGuides()).toEqual([20]);
+    expect(component.hoveredGuide()).toBeNull();
+    expect(preventSpy).toHaveBeenCalled();
+  });
+
+  it('should call dataservice.updateGraphDimensions', () => {
+    const fixture = TestBed.createComponent(GraphComponent);
+    const component = fixture.componentInstance;
+
+    const spy = spyOn(component['dataservice'], 'updateGraphDimensions');
+    component.updateGraphDimensions({ width: 100, height: 200 });
+
+    expect(spy).toHaveBeenCalledWith({ width: 100, height: 200 });
+  });
+
+  it('should return the correct default viewBox', () => {
+    const fixture = TestBed.createComponent(GraphComponent);
+    const component = fixture.componentInstance;
+
+    component['fullWidth'] = 800;
+    component['fullHeight'] = 600;
+
+    expect(component.defaultViewBox).toEqual({
+      x: 0,
+      y: 0,
+      width: 800,
+      height: 600
+    });
   });
 });
